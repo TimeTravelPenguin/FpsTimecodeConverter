90f7b95574a8a7344c6c5c574ae017d63f9ea7c4
Phillip Smith <FilupErwinSmith@gmail.com>
2020-02-21 00:42:39 +1100

    Initial push

---
+++ FramerateTimestampConverter/ViewModels/MainAppViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows;
+using FramerateTimestampConverter.WPF.Models;
+using Microsoft.Expression.Interactivity.Core;
+
+namespace FramerateTimestampConverter.WPF.ViewModels
+{
+  internal class MainAppViewModel
+  {
+    public FpsTimeCode FpsTime { get; } = new FpsTimeCode(60, 0);
+    public ActionCommand OpenGitHub { get; } = new ActionCommand(LaunchGitHub);
+
+    private static void LaunchGitHub()
+    {
+      var url = @"https://www.google.com";
+
+      try
+      {
+        Process.Start(url);
+      }
+      catch
+      {
+        // hack because of this: https://github.com/dotnet/corefx/issues/10361
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+          url = url.Replace("&", "^&", StringComparison.InvariantCulture);
+          Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") {CreateNoWindow = true});
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+          Process.Start("xdg-open", url);
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+          Process.Start("open", url);
+        }
+        else
+        {
+          MessageBox.Show("Check out the GitHub:" + Environment.NewLine + url, "Unable to automatically launch web browser");
+        }
+      }
+    }
+  }
+}
---
+++ FramerateTimestampConverter/Views/MainAppView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FramerateTimestampConverter.WPF.Views
+{
+  /// <summary>
+  /// Interaction logic for MainAppView.xaml
+  /// </summary>
+  public partial class MainAppView : Window
+  {
+    public MainAppView()
+    {
+      InitializeComponent();
+    }
+  }
+}
