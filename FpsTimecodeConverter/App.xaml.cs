edf1712a7e74a0ddf2a587c7df08413dada89354
Phillip Smith <FilupErwinSmith@gmail.com>
2020-03-01 13:40:00 +1100

    Moved from .NET Core to .NET Framework

--- FpsTimecodeConverter/App.xaml.cs
+++ FpsTimecodeConverter/App.xaml.cs
@@ -1,9 +1,25 @@
-﻿using System.Windows;
+﻿#region Title Header
+
+// Name: Phillip Smith
+// 
+// Solution: FpsTimecodeConverter
+// Project: FpsTimecodeConverter
+// File Name: App.xaml.cs
+// 
+// Current Data:
+// 2020-03-01 1:36 PM
+// 
+// Creation Date:
+// 2020-03-01 1:24 PM
+
+#endregion
+
+using System.Windows;
 
 namespace FpsTimecodeConverter
 {
   /// <summary>
-  /// Interaction logic for App.xaml
+  ///   Interaction logic for App.xaml
   /// </summary>
   public partial class App : Application
   {
